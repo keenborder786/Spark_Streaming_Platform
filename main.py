19dbbae5a180049ca49f0f0ee1822809d50d9d3e
keenborder786 <21110290@lums.edu.pk>
2022-11-28 18:59:30 +0500

    Testing for master-worker architecture

--- main.py
+++ main.py
@@ -11,13 +11,14 @@ from pyspark.sql.types import *
 from streaming.spark_engine import SparkProcessing
 from streaming.deltalake_engine import DeltaLakeInteraction
 from streaming.config import (spark_config,
-                    hadoop_config,
+                    # hadoop_config,
                     kafka_server,
                     topic_name,
                     sourceBucket,
                     kafka_config,
                     customer_table_config,
-                    fact_hash_id_table_config)
+                    fact_hash_id_table_config,
+                    MASTER_HOST_NAME)
 
 
 
@@ -81,34 +82,35 @@ def batch_function_customer_processing(micro_df:pyspark.sql.DataFrame, batch_id:
 if __name__ == '__main__':
 
     #Setting up the spark session
-    spark_processor = SparkProcessing('kafka_delta' , 'local[*]',spark_config,hadoop_config)
-    raw_events_deltalake_instance = DeltaLakeInteraction(spark_processor.spark_session, sourceBucket , 'FactHashIDs')
+    spark_processor = SparkProcessing('kafka_delta' , MASTER_HOST_NAME,spark_config,{})
+    
+    # raw_events_deltalake_instance = DeltaLakeInteraction(spark_processor.spark_session, sourceBucket , 'FactHashIDs')
 
-    #Reading kafka stream
+    # #Reading kafka stream
     df = spark_processor.read_kafka_stream(kafka_server, topic_name, 'latest',kafka_config)
-    
+    df.writeStream.format('console').start().awaitTermination()
 
-    #### Processing the raw_events coming from kafka. Taking out the unique_message_id and payload which contains the values for our table.
-    raw_events = spark_processor.event_processing(df)
+    # #### Processing the raw_events coming from kafka. Taking out the unique_message_id and payload which contains the values for our table.
+    # raw_events = spark_processor.event_processing(df)
     
-    ##### Create the FactHashID tables if it does not exists. FactHashID will consist of hashes of messages_ids that have been processed so far.
-    raw_events_deltalake_instance = DeltaLakeInteraction(spark_processor.spark_session, sourceBucket , 'FactHashIDs')
-    raw_events_table = raw_events_deltalake_instance.create_delta_table(raw_events.select('unique_message_id').schema, fact_hash_id_table_config)
+    # ##### Create the FactHashID tables if it does not exists. FactHashID will consist of hashes of messages_ids that have been processed so far.
+    # raw_events_deltalake_instance = DeltaLakeInteraction(spark_processor.spark_session, sourceBucket , 'FactHashIDs')
+    # raw_events_table = raw_events_deltalake_instance.create_delta_table(raw_events.select('unique_message_id').schema, fact_hash_id_table_config)
     
-    ##### Processing the customer data from payload.
-    customer_update = spark_processor.customer_table_processing(raw_events.select('after_payload'))
+    # ##### Processing the customer data from payload.
+    # customer_update = spark_processor.customer_table_processing(raw_events.select('after_payload'))
 
-    #### Create the customer table if it does not exists
-    customer_table_deltalake_instance = DeltaLakeInteraction(spark_processor.spark_session, sourceBucket , 'DimCustomer')
-    customer_table = customer_table_deltalake_instance.create_delta_table(customer_update.schema, customer_table_config)
+    # #### Create the customer table if it does not exists
+    # customer_table_deltalake_instance = DeltaLakeInteraction(spark_processor.spark_session, sourceBucket , 'DimCustomer')
+    # customer_table = customer_table_deltalake_instance.create_delta_table(customer_update.schema, customer_table_config)
 
-    #### Writing the raw_events on delta lake and ensuring each message is only written once by using a unique identifier
-    ### We only write the hash values to our FactHashIDs table. 
-    raw_event_streaming = raw_events.select(['unique_message_id']).repartition(1).writeStream.foreachBatch(batch_function_raw_events).outputMode("append") \
-        .option("checkpointLocation", "s3a://{}/{}/_checkpoint".format(sourceBucket,'FactHashIDs')) \
-        .start()
+    # #### Writing the raw_events on delta lake and ensuring each message is only written once by using a unique identifier
+    # ### We only write the hash values to our FactHashIDs table. 
+    # raw_event_streaming = raw_events.select(['unique_message_id']).repartition(1).writeStream.foreachBatch(batch_function_raw_events).outputMode("append") \
+    #     .option("checkpointLocation", "s3a://{}/{}/_checkpoint".format(sourceBucket,'FactHashIDs')) \
+    #     .start()
     
-    ##### Updating the customer table data on delta lake from our new messages
-    customer_table_streaming = customer_update.repartition(1).writeStream.foreachBatch(batch_function_customer_processing).outputMode("update") \
-        .option("checkpointLocation", "s3a://{}/{}/_checkpoint".format(sourceBucket,'DimCustomer')) \
-        .start().awaitTermination()
+    # ##### Updating the customer table data on delta lake from our new messages
+    # customer_table_streaming = customer_update.repartition(1).writeStream.foreachBatch(batch_function_customer_processing).outputMode("update") \
+    #     .option("checkpointLocation", "s3a://{}/{}/_checkpoint".format(sourceBucket,'DimCustomer')) \
+    #     .start().awaitTermination()
--- streaming/config.py
+++ streaming/config.py
@@ -12,24 +12,27 @@ kafka_config = json.loads(os.environ['KafkaConsumerConfig'])
 topic_name = os.environ['TopicName']
 customer_table_config = json.loads(os.environ['CustomerTableConfig'])
 fact_hash_id_table_config = json.loads(os.environ['RawEventTableConfig'])
+MASTER_HOST_NAME = os.environ['MASTER_HOST_NAME']
+DRIVER_HOST_NAME = os.environ['DRIVER_HOST_NAME']
 
-
-spark_config={"spark.sql.extensions":"io.delta.sql.DeltaSparkSessionExtension",
-                "spark.sql.catalog.spark_catalog":"org.apache.spark.sql.delta.catalog.DeltaCatalog",
-                "spark.executor.extraJavaOptions":"-Dcom.amazonaws.services.s3.enableV4=true",
-                "spark.driver.extraJavaOptions":"-Dcom.amazonaws.services.s3.enableV4=true",
+spark_config={
+                # "spark.sql.catalog.spark_catalog":"org.apache.spark.sql.delta.catalog.DeltaCatalog",
+                # "spark.executor.extraJavaOptions":"-Dcom.amazonaws.services.s3.enableV4=true",
+                # "spark.driver.extraJavaOptions":"-Dcom.amazonaws.services.s3.enableV4=true",
                 "spark.streaming.receiver.writeAheadLog.enable":"true",
                 "spark.streaming.driver.writeAheadLog.closeFileAfterWrite":"true",
                 "spark.streaming.receiver.writeAheadLog.closeFileAfterWrite":"true",
-                "spark.databricks.delta.vacuum.parallelDelete.enabled":"true",
-                "spark.databricks.delta.optimize.repartition.enabled":"true"
+                "spark.driver.host":DRIVER_HOST_NAME
+                # "spark.databricks.delta.vacuum.parallelDelete.enabled":"true",
+                # "spark.databricks.delta.optimize.repartition.enabled":"true"
+                # "spark.sql.extensions":"io.delta.sql.DeltaSparkSessionExtension",
                 }
-hadoop_config={"fs.s3a.endpoint":s3endPointLoc,
-                "fs.s3a.access.key":s3accessKeyAws,
-                "fs.s3a.secret.key":s3secretKeyAws,
-                "spark.hadoop.fs.s3a.aws.credentials.provider":"org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider",
-                "spark.hadoop.fs.s3a.path.style.access":"true",
-                "com.amazonaws.services.s3.enableV4":"true",
-                "fs.s3a.connection.ssl.enabled":"false",
-                "spark.hadoop.fs.s3a.impl":"org.apache.hadoop.fs.s3a.S3AFileSystem"
-                }
+# hadoop_config={"fs.s3a.endpoint":s3endPointLoc,
+#                 "fs.s3a.access.key":s3accessKeyAws,
+#                 "fs.s3a.secret.key":s3secretKeyAws,
+#                 "spark.hadoop.fs.s3a.aws.credentials.provider":"org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider",
+#                 "spark.hadoop.fs.s3a.path.style.access":"true",
+#                 "com.amazonaws.services.s3.enableV4":"true",
+#                 "fs.s3a.connection.ssl.enabled":"false",
+#                 "spark.hadoop.fs.s3a.impl":"org.apache.hadoop.fs.s3a.S3AFileSystem"
+#                 }
